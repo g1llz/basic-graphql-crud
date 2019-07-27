0c7773bdd1296b2d277a2455fff05cc32c947870
Gilles Lima <gillestoim@gmail.com>
2019-07-27 10:56:02 -0300

    create server, schema.graphql and connect mongoose

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const { GraphQLServer } = require('graphql-yoga');
+const mongoose = require('mongoose');
+const path = require('path');
+const resolvers = require('./resolvers');
+
+mongoose.connect('mongodb://localhost:27017/graphqlcrud', {
+  useNewUrlParser: true,
+});
+
+const server = new GraphQLServer({
+  typeDefs: path.resolve(__dirname, 'schema.graphql'),
+  resolvers
+});
+
+server.start();
